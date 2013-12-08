dd4c704fc55d05399c078e408c5e2417fc94bd37
Dig <dig.s.vargas@gmail.com>
2013-12-07 18:04:14 -0800

    Started on Associatable

--- lib/active_record_lite/associatable.rb
+++ lib/active_record_lite/associatable.rb
@@ -13,6 +13,20 @@ end
 
 class BelongsToAssocParams < AssocParams
   def initialize(name, params)
+    @name = name
+    expected_params = [:class_name, :foreign_key, :primary_key]
+    expected_params.each do |expected|
+      if params.keys.include?(expected)
+        instance_variable_set("@#{expected.to_s}", params[expected])
+      elsif expected == :primary_key
+        instance_variable_set("@#{expected.to_s}", "id")
+      elsif expected == :foreign_key
+        instance_variable_set("@#{expected.to_s}", "foreign")
+      elsif expected == :class_name
+        instance_variable_set("@#{expected.to_s}", @name.classify)
+      end
+    end
+
   end
 
   def type
