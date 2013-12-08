cdeacca5d3ca036b665a1c0663c454cfcd9d6673
Dig <dig.s.vargas@gmail.com>
2013-12-07 16:35:32 -0800

    Create worked once, debugging, update in process

--- lib/active_record_lite/sql_object.rb
+++ lib/active_record_lite/sql_object.rb
@@ -2,6 +2,8 @@ require_relative './associatable'
 require_relative './db_connection' # use DBConnection.execute freely here.
 require_relative './mass_object'
 require_relative './searchable'
+require 'debugger'
+
 
 class SQLObject < MassObject
   # sets the table_name
@@ -50,20 +52,42 @@ class SQLObject < MassObject
   # use send and map to get instance values.
   # after, update the id attribute with the helper method from db_connection
   def create
-    DBConnection.execute(<<-SQL)
+    attr_count = self.class.attributes.count
+    question_mark_string = (['?'] * attr_count).join(", ")
+    attr_vals = self.class.attributes.map { |attr| self.send(attr) }
 
+    DBConnection.execute(<<-SQL, *attr_vals)
+      INSERT INTO
+        "#{self.class.table_name}" (#{self.class.attributes.join(", ")})
+      VALUES
+        (#{question_mark_string})
 
-      
     SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   # executes query that updates the row in the db corresponding to this instance
   # of the class. use "#{attr_name} = ?" and join with ', ' for set string.
   def update
+    attr_vals = self.class.attributes.map { |attr| self.send(attr) }
+    attr_string_parts = self.class.attributes.map { |attr| "#{attr} = ?" } 
+    set_lines = attr_string_parts.join(", ")
+
+    DBConnection.execute(<<-SQL, *attr_vals)
+      UPDATE
+        "#{self.class.table_name}"
+      SET
+        (set_lines)
+      WHERE
+        self.id = id
+
+    SQL
   end
 
   # call either create or update depending if id is nil.
   def save
+    self.nil? ? self.create : self.update
   end
 
   # helper method to return values of the attributes.
--- test/ruby_sql_object_test.rb
+++ test/ruby_sql_object_test.rb
@@ -1,18 +1,21 @@
 require_relative '../lib/active_record_lite'
+require 'debugger'
 
 # https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
 cats_db_file_name =
-  File.expand_path(File.join(File.dirname(__FILE__), "cats.db"))
+  File.expand_path(File.join(File.dirname(__FILE__), "../spec/cats.db"))
 DBConnection.open(cats_db_file_name)
 
 class Cat < SQLObject
   set_table_name("cats")
   my_attr_accessible(:id, :name, :owner_id)
+  my_attr_accessor(:id, :name, :owner_id)
 end
 
 class Human < SQLObject
   set_table_name("humans")
   my_attr_accessible(:id, :fname, :lname, :house_id)
+  my_attr_accessor(:id, :fname, :lname, :house_id)
 end
 
 p Human.find(1)
@@ -24,6 +27,7 @@ p Cat.all
 
 c = Cat.new(:name => "Gizmo", :owner_id => 1)
 c.save
+p c.id
 
 h = Human.find(1)
 # just run an UPDATE; no values changed, so shouldnt hurt the db
